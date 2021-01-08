186b462a2d92f66e99e93d0ddbf67692e8dab2b6
Carly Malatskey <cmalatskey@gmail.com>
2021-01-09 00:19:23 +0200

    added find a pet button on logged in home

--- src/components/Home/LoggedInHome/LoggedInHome.js
+++ src/components/Home/LoggedInHome/LoggedInHome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./LoggedInHome.css";
 
 class LoggedInHome extends React.Component {
     constructor(props) { 
@@ -15,6 +16,9 @@ class LoggedInHome extends React.Component {
                         who are just looking for a loving and warm environment where
                         they feel safe and secure.
                     </h4>
+                    <div>
+                        <Link className="find-a-pet-button" to="/search">Find a Pet</Link> 
+                    </div>
                 </div>
             </div>
         );
